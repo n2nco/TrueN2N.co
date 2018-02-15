2faf31fc15ab3cd0da0ab278c5e91064cd29ffd1
Osklük <36323943+n2nco@users.noreply.github.com>
2018-02-15 00:02:56 -0500

    About.js minor updates - 1st iteration

--- src/About.js
+++ src/About.js
@@ -25,14 +25,14 @@ class About extends Component {
         <h1 className="About-header">About</h1>
         </Center>
         <Center>
-        <p className="cypherpunk">N:N writes code, propagates talent & allocates [crypto]capital. </p>
+        <p className="cypherpunk">N:N is entrepreneurs. We write code, propagate talent & allocates [crypto]capital. </p>
         </Center>
 
         <h2 className="About-header">Domain of focus: </h2>
         <p className="cypherpunk">
-        Current: Intersection of decentralized (dx) protocols & AI.
-        Sub-domain: Unlocking N:N value efficiency (scale, scope etc.) in n dimensions.
-        This encompasses: dx Identity. Delegating dx trust. Protocol liquidity provision. 
+        Current: Intersection of decentralized (dx) protocols & AI. <br>
+        Sub-domain: Unlocking N:N value efficiency (scale, scope etc.) in n dimensions. <br>
+        This encompasses: dx Identity. Delegating dx trust. Protocol market making & liquidity provision. <br>
         Future: Acting quick where nascent whitespace of inefficiency lie. (subject to change dynamically).
         The current state of most domains = overvalued efficiency > dynamism, anti-fragility.</p>
 
@@ -46,15 +46,15 @@ class About extends Component {
         <p className="cypherpunk">ERC20 Token [ETH] vs. Non-Fungible Asset. [Home Equity]</p>
         <p className="cypherpunk">Debt [Collaterized Debt Position] vs. Bag [TRON]</p>
 
-        <h2 className="About-header">What is a Collective?</h2>
-        <p className="cypherpunk">Free flowing group of individuals with access to resources with a shared mission to create.</p>
+        <h2 className="About-header">Collective?</h2>
+        <p className="cypherpunk">Free flowing group of individuals with access to foce-multiplying resources & a shared creation ethos.</p>
 
         <p className="cypherpunk">Our conjecture: trajectory will be primarily be determined by:
-        a) Data access/flow; Networks [hardware & wetware]. 
+        a) Data access/flow; Networks [hardware & wetware]. <br>
         b) Intelligence; AI</p>
         
         <h2 className="About-header">What?</h2>
-        <p className="cypherpunk">Building is underway. Non-fungible asset networks. Distributed debt/equity/value protocols. </p>
+        <p className="cypherpunk">Building is underway. Examples: Non-fungible asset networks. Distributed value/debt/equity protocols. </p>
 
         <h2 className="About-header">How?</h2>
         <p className="cypherpunk">
